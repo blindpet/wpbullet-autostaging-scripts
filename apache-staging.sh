0df5269228119ba99314459a3e92458192afa702
WP Bullet <wpbulletplugins@gmail.com>
2016-10-09 18:00:46 +0200

    Update apache-staging.sh
    ninjafix for de-indexing

--- apache-staging.sh
+++ apache-staging.sh
@@ -92,7 +92,7 @@ sudo -u www-data wp db import /tmp/$STAGINGDOMAIN.sql --path=$STAGINGPATH --skip
 #sudo -u www-data wp db import /tmp/$STAGINGDOMAIN-url.sql --path=$STAGINGPATH --skip-themes --skip-plugins
 sudo -u www-data wp search-replace $EXTRACTEDPATH $STAGINGPATH --path=$STAGINGPATH --skip-themes --skip-plugins
 #turn off indexing of search engins
-sudo -u www-data wp option update blog_public 1
+sudo -u www-data wp option update blog_public 0 --path=$STAGINGPATH --skip-themes --skip-plugins
 
 sudo rm /tmp/$STAGINGDOMAIN.sql
 
