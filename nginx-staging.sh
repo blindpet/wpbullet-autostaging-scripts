aaa806a52c53377f7167043ae54f81409e6a3f44
wpbullet <wpbulletplugins@gmail.com>
2016-10-11 22:36:16 +0200

    added comments nginx and cleanup

--- nginx-staging.sh
+++ nginx-staging.sh
@@ -10,26 +10,37 @@ if [ $(id -u) != "0" ]; then
     exit 1
 fi
 
-#check wp-cli present and install
-if [ ! hash wp 2>/dev/null]; then
+# check wp-cli present and install
+if [ ! hash wp 2>/dev/null ]; then
     sudo wget -q https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O /usr/bin/wp
     sudo chmod 755 /usr/bin/wp
 fi
-#define mysql root password
+
+# define mysql root password
 MYSQLROOTPASS=
-#user to run WP CLI as
+
+# user to run WP CLI as
 WPCLIUSER="www-data"
+
+# path to nginx virtual hosts
 NGINXSITEPATH=/etc/nginx/sites-available
+
+# path to enabled nginx virtual hosts
 NGINXSITESENABLED=/etc/nginx/sites-enabled
+
+# path to WordPress installations
 SITEPATH=/var/www
+
+# create array of virtual hosts
 SITELIST=($(ls -lh $NGINXSITEPATH | awk '{print $9}'))
-#generate hash based on date and use first 8 characters for subdomain
+
+# generate hash based on date and use first 8 characters for subdomain
 NEWHASH=$(date | sha1sum | awk '{ print substr($0,0,8)}')
 
-#capture first parameter
+# capture first parameter
 VHOST="$1"
 
-#if no parameter passed prompt user
+# if no parameter passed prompt user
 if [ -z "$VHOST" ]; then
     echo "What is the name of your virtual host?"
     echo "Virtual hosts found:\n ${SITELIST[@]} "
@@ -42,19 +53,21 @@ if [ ! -f $NGINXSITEPATH/$VHOST ]; then
     exit
 fi
 
+# define original vhost full path
 ORIGINALVHOST=$NGINXSITEPATH/$VHOST
-EXTRACTEDPATH=$(grep "root " $NGINXSITEPATH/$VHOST | awk '{print $2}' | tr -d ";")
+# extract path
+EXTRACTEDPATH=$(grep "root " $ORIGINALVHOST | grep -v "#" | awk '{print $2}' | tr -d ";")
 # server_name without www
-EXTRACTEDDOMAIN=$(grep server_name $NGINXSITEPATH/$VHOST | awk '{ gsub("www\.", ""); print $2 }' | tr -d ";")
+EXTRACTEDDOMAIN=$(grep server_name $ORIGINALVHOST | grep -v "#" | awk '{ gsub("www\.", ""); print $2 }' | tr -d ";")
 #extract siteurl
 SITEURL=$(wp option get siteurl --path=$EXTRACTEDPATH --skip-plugins --skip-themes --allow-root | awk -F ["\/"] '{ print $3 }')
 
-#make unique staging name for DNS and new path
+# make unique staging name for DNS and new path
 STAGINGDOMAIN=$NEWHASH.$EXTRACTEDDOMAIN
 STAGINGPATH="$SITEPATH/$STAGINGDOMAIN"
 STAGINGVHOST="$NGINXSITEPATH/$STAGINGDOMAIN"
 
-#multisitetest
+# multisitetest
 MULSTISITETEST=(grep "MULTISITE" $EXTRACTEDPATH/wp-config.php)
 
 if [ ! -z "$MULTISITETEST" ]; then
@@ -66,20 +79,19 @@ fi
 # nginx stuff
 ###
 
-##new nginx virtual host
+# new nginx virtual host
 
-cp $NGINXSITEPATH/$VHOST $STAGINGVHOST
+cp $ORIGINALVHOST $STAGINGVHOST
 ln -s $STAGINGVHOST $NGINXSITESENABLED/$STAGINGDOMAIN
 
-#replace old server_name with new server_name
+# replace old server_name with new server_name
 sed -i "/server_name/c\server_name $STAGINGDOMAIN;" $STAGINGVHOST
 
-#replace access and log names use full line replacement
+# replace access and log names use full line replacement
 sed -i "/access_log/c\access_log   /var/log/nginx/$STAGINGDOMAIN.access.log;" $STAGINGVHOST
 sed -i "/error_log/c\access_log   /var/log/nginx/$STAGINGDOMAIN.error.log;" $STAGINGVHOST
 
-#root folder replacement
-# sed -i '/^root/c\root herp;' /etc/nginx/sites-enabled/63f1a28.wp-bullet.online
+# root folder replacement
 sed -i "/root /c\root $STAGINGPATH;" $STAGINGVHOST
 
 #copy over wordpress folder
@@ -89,7 +101,7 @@ cp -r $EXTRACTEDPATH $STAGINGPATH
 # MySQL stuff
 ###
 
-#extract original DB stuff
+#vextract original DB stuff
 OLDDB=$(grep DB_NAME $EXTRACTEDPATH/wp-config.php | awk -F ["\'"] '{ print $4 }')
 OLDDBUSER=$(grep DB_USER $EXTRACTEDPATH/wp-config.php | awk -F ["\'"] '{ print $4 }')
 OLDDBPASS=$(grep DB_PASSWORD $EXTRACTEDPATH/wp-config.php | awk -F ["\'"] '{ print $4 }')
@@ -97,42 +109,40 @@ NEWDB=$OLDDB$NEWHASH
 NEWDBUSER=$OLDDBUSER$NEWHASH
 NEWDBPASS=$OLDDBPASS$NEWHASH
 
-#export original db and search replace 
+# export original db and search replace
 sudo -u $WPCLIUSER wp search-replace $SITEURL $STAGINGDOMAIN --export=/tmp/$STAGINGDOMAIN.sql --path=$EXTRACTEDPATH --skip-themes --skip-plugins
-#sudo -u www-data wp search-replace $EXTRACTEDDOMAIN $STAGINGDOMAIN '*_options' --export=/tmp/$STAGINGDOMAIN-url.sql --path=$EXTRACTEDPATH --skip-themes --skip-plugins
 
-#create new db user, pass
+# create new db user, pass
 mysql -u root -p${MYSQLROOTPASS} -e "CREATE USER ${NEWDBUSER}@localhost IDENTIFIED BY '${NEWDBPASS}';"
 mysql -u root -p${MYSQLROOTPASS} -e "CREATE DATABASE ${NEWDB};"
 mysql -u root -p${MYSQLROOTPASS} -e "GRANT ALL PRIVILEGES ON ${NEWDB}.* TO ${NEWDBUSER}@localhost IDENTIFIED BY '${NEWDBPASS}';"
 mysql -u root -p${MYSQLROOTPASS} -e "FLUSH PRIVILEGES;"
 
-#replace staging db info
-
+# replace staging db info
 sed -i "/define('DB_NAME', /c\define('DB_NAME', '${NEWDB}');" ${STAGINGPATH}/wp-config.php
 sed -i "/define('DB_USER', /c\define('DB_USER', '${NEWDBUSER}');" ${STAGINGPATH}/wp-config.php
 sed -i "/define('DB_PASSWORD', /c\define('DB_PASSWORD', '${NEWDBPASS}');" ${STAGINGPATH}/wp-config.php
 
-#permissions fix
-
-chown -R $WPCLIUSER:$WPCLIUSER $STAGINGPATH/
+# permissions fix
+chown -R $WPCLIUSER:$WPCLIUSER $STAGINGPATH
 find $STAGINGPATH -type f -exec chmod 644 {} +
 find $STAGINGPATH -type d -exec chmod 755 {} +
 
-#import new db
-
+# import new db
 sudo -u $WPCLIUSER wp db import /tmp/$STAGINGDOMAIN.sql --path=$STAGINGPATH --skip-themes --skip-plugins
-#sudo -u www-data wp db import /tmp/$STAGINGDOMAIN-url.sql --path=$STAGINGPATH --skip-themes --skip-plugins
+
+# replace paths
 sudo -u $WPCLIUSER wp search-replace $EXTRACTEDPATH $STAGINGPATH --path=$STAGINGPATH --skip-themes --skip-plugins
-#turn off indexing of search engines
+
+# turn off indexing of search engines
 sudo -u $WPCLIUSER wp option update blog_public 0 --path=$STAGINGPATH --skip-themes --skip-plugins
 
-#remove dump
+# remove db dump
 sudo rm /tmp/$STAGINGDOMAIN.sql
 
-#reload nginx
+# reload nginx
 service nginx reload
 
-#print results
+# print results
 echo "Staging domain is $STAGINGDOMAIN"
 echo "Staging path is $STAGINGPATH"
