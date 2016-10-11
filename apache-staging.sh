2b2057abed8c8970e29eb769077c44d531959a4a
wpbullet <wpbulletplugins@gmail.com>
2016-10-11 22:38:33 +0200

    cleanup apache comments

--- apache-staging.sh
+++ apache-staging.sh
@@ -11,61 +11,61 @@ if [ $(id -u) != "0" ]; then
 fi
 
 #check wp-cli present and install
-if [ ! hash wp 2>/dev/null]; then
+if [ ! hash wp 2>/dev/null ]; then
     sudo wget -q https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O /usr/bin/wp
     sudo chmod 755 /usr/bin/wp
 fi
 
-#define mysql root password
+# define mysql root password
 MYSQLROOTPASS=
 
-#user to run wp cli as
+# user to run wp cli as
 WPCLIUSER="www-data"
 
-#define path to Apache virtual hosts
+# define path to Apache virtual hosts
 APACHESITEPATH=/etc/apache2/sites-available
 
-#where WordPress sites are
+# where WordPress sites are
 SITEPATH=/var/www
 
-#generate array of sites to display to user
+# generate array of sites to display to user
 SITELIST=($(ls -lh $APACHESITEPATH | awk '{print $9}'))
 
-#generate hash based on date and use first 8 characters for subdomain
+# generate hash based on date and use first 8 characters for subdomain
 NEWHASH=$(date | sha1sum | awk '{ print substr($0,0,8)}')
 
-#capture first parameter which is the vhost file
+# capture first parameter which is the vhost file
 VHOST="$1"
 
-#if no parameter passed prompt user for vhost
+# if no parameter passed prompt user for vhost
 if [ -z "$VHOST" ]; then
     echo "What is the name of your virtual host?"
     echo "Virtual hosts found:\n ${SITELIST[@]} "
     read VHOST
 fi
 
-#check if vhost exists
+# check if vhost exists
 if [ ! -f $APACHEPATH/$VHOST ]; then
     echo "$VHOST not found"
     exit
 fi
 
-#define the vhost
+# define the vhost
 ORIGINALVHOST=$APACHESITEPATH/$VHOST
 
-#get path of WordPress install
+# get path of WordPress install
 EXTRACTEDPATH=$(grep DocumentRoot $ORIGINALVHOST | grep -v "#" | awk '{ print $2 }')
 
 # server_name without www
 EXTRACTEDDOMAIN=$(grep ServerName $ORIGINALVHOST | grep -v "#" | awk '{ gsub("www\.", ""); print $2 }')
 SITEURL=$(wp option get siteurl --path=$EXTRACTEDPATH --skip-plugins --skip-themes --allow-root | awk -F ["\/"] '{ print $3 }')
 
-#make unique staging name for DNS and new path
+# make unique staging name for DNS and new path
 STAGINGDOMAIN=$NEWHASH.$EXTRACTEDDOMAIN
 STAGINGPATH="$SITEPATH/$STAGINGDOMAIN"
 STAGINGVHOST="$APACHESITEPATH/$STAGINGDOMAIN.conf"
 
-#multisitetest
+# multisitetest
 MULSTISITETEST=(grep "MULTISITE" $EXTRACTEDPATH/wp-config.php)
 
 if [ ! -z "$MULTISITETEST" ]; then
@@ -73,23 +73,23 @@ if [ ! -z "$MULTISITETEST" ]; then
     exit
 fi
 
-#copy Apache virtual host
+# copy Apache virtual host
 cp $ORIGINALVHOST $STAGINGVHOST
 
-#replace old server_name with new server_name
+# replace old server_name with new server_name
 sed -i "/ServerName /c\ServerName $STAGINGDOMAIN" $STAGINGVHOST
 
-#root folder replacement
+# root folder replacement
 sed -i "/DocumentRoot /c\DocumentRoot $STAGINGPATH" $STAGINGVHOST
 
-#copy over wordpress folder
+# copy over wordpress folder
 cp -r $EXTRACTEDPATH $STAGINGPATH
 
 ###
 # MySQL stuff
 ###
 
-#extract original DB stuff
+# extract original DB stuff
 OLDDB=$(grep DB_NAME $EXTRACTEDPATH/wp-config.php | awk -F ["\'"] '{ print $4 }')
 OLDDBUSER=$(grep DB_USER $EXTRACTEDPATH/wp-config.php | awk -F ["\'"] '{ print $4 }')
 OLDDBPASS=$(grep DB_PASSWORD $EXTRACTEDPATH/wp-config.php | awk -F ["\'"] '{ print $4 }')
@@ -97,42 +97,42 @@ NEWDB=$OLDDB$NEWHASH
 NEWDBUSER=$OLDDBUSER$NEWHASH
 NEWDBPASS=$OLDDBPASS$NEWHASH
 
-#export original db and search replace 
+# export original db and search replace
 sudo -u $WPCLIUSER wp search-replace $SITEURL $STAGINGDOMAIN --export=/tmp/$STAGINGDOMAIN.sql --path=$EXTRACTEDPATH --skip-themes --skip-plugins
 #sudo -u www-data wp search-replace $EXTRACTEDDOMAIN $STAGINGDOMAIN '*_options' --export=/tmp/$STAGINGDOMAIN-url.sql --path=$EXTRACTEDPATH --skip-themes --skip-plugins
 
-#create new db user, pass
+# create new db user, pass
 mysql -u root -p${MYSQLROOTPASS} -e "CREATE USER ${NEWDBUSER}@localhost IDENTIFIED BY '${NEWDBPASS}';"
 mysql -u root -p${MYSQLROOTPASS} -e "CREATE DATABASE ${NEWDB};"
 mysql -u root -p${MYSQLROOTPASS} -e "GRANT ALL PRIVILEGES ON ${NEWDB}.* TO ${NEWDBUSER}@localhost IDENTIFIED BY '${NEWDBPASS}';"
 mysql -u root -p${MYSQLROOTPASS} -e "FLUSH PRIVILEGES;"
 
-#replace staging db info
+# replace staging db info
 
 sed -i "/define('DB_NAME', /c\define('DB_NAME', '${NEWDB}');" ${STAGINGPATH}/wp-config.php
 sed -i "/define('DB_USER', /c\define('DB_USER', '${NEWDBUSER}');" ${STAGINGPATH}/wp-config.php
 sed -i "/define('DB_PASSWORD', /c\define('DB_PASSWORD', '${NEWDBPASS}');" ${STAGINGPATH}/wp-config.php
 
-#permissions fix
+# permissions fix
 
 chown -R $WPCLIUSER:$WPCLIUSER $STAGINGPATH
 find $STAGINGPATH -type f -exec chmod 644 {} +
 find $STAGINGPATH -type d -exec chmod 755 {} +
 
-#import new db
+# import new db
 
 sudo -u $WPCLIUSER wp db import /tmp/$STAGINGDOMAIN.sql --path=$STAGINGPATH --skip-themes --skip-plugins
-#update paths
+# update paths
 sudo -u $WPCLIUSER wp search-replace $EXTRACTEDPATH $STAGINGPATH --path=$STAGINGPATH --skip-themes --skip-plugins
-#turn off indexing of search engines
+# turn off indexing of search engines
 sudo -u $WPCLIUSER wp option update blog_public 0 --path=$STAGINGPATH --skip-themes --skip-plugins
 
 sudo rm /tmp/$STAGINGDOMAIN.sql
 
-#reload Apache
+# reload Apache
 a2ensite $STAGINGDOMAIN
 service apache2 reload
 
-#print results
+# print results
 echo "Staging domain is $STAGINGDOMAIN"
 echo "Staging path is $STAGINGPATH"
